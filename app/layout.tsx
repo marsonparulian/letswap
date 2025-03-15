7d3b2fc9715924f2c275e2b4df11cbed715f8e4a
mpa <marson.parulian@gmail.com]>
2025-03-15 16:09:33 +1100

    Include Foundation library

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,16 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+
+        {/* Foundation CSS */}
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.9.0/dist/css/foundation.min.css" />
+
+        {/* JQuery JS */}
+        <script src="https://code.jquery.com/jquery-3.7.0.min.js" ></script>
+
+        {/* Foundation JSF */}
+        <script src="https://cdn.jsdelivr.net/npm/foundation-sites@6.9.0/dist/js/foundation.min.js"></script>
+
       </body>
     </html>
   );
