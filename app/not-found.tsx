7df03837c62ce9456fed501032b3c2d84ce1287c
mpa <marson.parulian@gmail.com]>
2025-03-22 15:05:16 +1100

    UI: add 404 error page

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+// Globl 404 page
+
+export default function NotFoundPage() {
+    return (
+        <div className="grid-container">
+            <div className="grid-x grid-padding-x align-center-middle text-center">
+                <div className="cell medium-6">
+                    <h1 className="alert alert-danger">Not Found</h1>
+                    <p className="alert red">The page you are looking for does not exist.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
