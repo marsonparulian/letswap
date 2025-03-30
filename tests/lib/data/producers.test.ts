7193aaa6731ee03737168171b06a54c20d40a693
mpa <marson.parulian@gmail.com]>
2025-03-31 07:39:09 +1100

    Test: Fix: Set testEnvironment to `node` in DB related test

--- tests/lib/data/producers.test.ts
+++ tests/lib/data/producers.test.ts
@@ -1,4 +1,9 @@
+/**
+ * @jest-environment node
+*/
+// Note: directive above is needed to override the global `jsdom`, since this file tests server functions (DB related)
 // Description: Test for the producers data layer
+
 
 import { tableName, deleteProducer } from '@/app/lib/data/producers';
 
@@ -6,12 +11,12 @@ describe('lib/data/producers', () => {
     it('Should have the correct table name', () => {
         expect(tableName).toBe('producers');
     });
-    // describe('Delete producers', () => {
+    describe('Delete producers', () => {
 
-    // it('Should delete by id', async () => {
-    //     const id = '1';
-    //     const result = await deleteProducer(id);
-    //     expect(result).toBe(true);
-    // })
-    // });
+        it('Should delete by id', async () => {
+            const id = '1';
+            const result = await deleteProducer(id);
+            expect(result).toBe(true);
+        })
+    });
 });
