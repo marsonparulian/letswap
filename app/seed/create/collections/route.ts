d8a5e196b4b29182acc5fcfa6e9e2ae2db489e27
mpa <marson.parulian@gmail.com]>
2025-04-07 23:04:57 +1000

    Fix: error in table name when creating table

--- app/seed/create/collections/route.ts
+++ app/seed/create/collections/route.ts
@@ -1,31 +1,34 @@
 // Purpose : Create collections table.
-import { tableName } from '@/app/lib/data/collection_items';
+import { tableName } from '@/app/lib/data/collections';
 import { sql } from '@/app/lib/data/utils';
 
 async function createCollections() {
     return sql`
-    CREATE TABLE IF NOT EXISTS ${sql(tableName)}(
-      id SERIAL PRIMARY KEY,
-      slug VARCHAR(255) NOT NULL UNIQUE,
-      name VARCHAR(255) NOT NULL,
-      description TEXT NOT NULL,
-      items_count INT NOT NULL,
-      producer_id INT NOT NULL REFERENCES producers(id),
-      image_url VARCHAR(255) NOT NULL,
-      year INT CHECK (year >= 1000 AND year <= 9999),
-      created_at TIMESTAMP NOT NULL DEFAULT NOW()
-    );
-  `;
+        CREATE TABLE IF NOT EXISTS ${sql(tableName)}(
+                id SERIAL PRIMARY KEY,
+                slug VARCHAR(255) NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                description TEXT,
+                items_count INT NOT NULL,
+                producer_id INT NOT NULL REFERENCES producers(id),
+                image_url VARCHAR(255),
+                year INT CHECK (year >= 1000 AND year <= 9999),
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+      `;
 }
 
 export async function GET() {
     try {
+        console.log('start of creating table');
         await sql.begin(() => [
             createCollections(),
         ]);
 
         return Response.json({ message: `Table ${tableName} has been created` });
     } catch (error) {
+        console.log('error creating table');
+        console.log(error);
         return Response.json({ error }, { status: 500 });
     }
 }
