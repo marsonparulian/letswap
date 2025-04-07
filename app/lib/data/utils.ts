ab7c0b355db49386ddc2b596f4724709197f5472
mpa <marson.parulian@gmail.com]>
2025-04-07 23:06:06 +1000

    DB: add function to close connection

--- app/lib/data/utils.ts
+++ app/lib/data/utils.ts
@@ -2,3 +2,7 @@
 import postgres from 'postgres';
 
 export const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+export const closeSqlConnection = async () => {
+    await sql.end({ timeout: 5 });
+};
