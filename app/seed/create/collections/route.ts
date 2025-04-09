35b69bb667d96afb2ad68bccf95655fb86b193e3
mpa <marson.parulian@gmail.com]>
2025-04-09 14:05:00 +1000

    DB: Change `description` to `desc`

--- app/seed/create/collections/route.ts
+++ app/seed/create/collections/route.ts
@@ -3,12 +3,15 @@ import { tableName } from '@/app/lib/data/collections';
 import { sql } from '@/app/lib/data/utils';
 
 async function createCollections() {
+    // Check if the table already exists
+    await sql`DROP TABLE IF EXISTS ${sql(tableName)};`;
+    // Create the table
     return sql`
         CREATE TABLE IF NOT EXISTS ${sql(tableName)}(
                 id SERIAL PRIMARY KEY,
                 slug VARCHAR(255) NOT NULL,
                 name VARCHAR(255) NOT NULL,
-                description TEXT,
+                desc TEXT,
                 items_count INT NOT NULL,
                 producer_id INT NOT NULL REFERENCES producers(id),
                 image_url VARCHAR(255),
