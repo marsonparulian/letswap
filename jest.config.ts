4ab398debf2603ac3d51958dcbfe98bf9875dfb6
mpa <marson.parulian@gmail.com]>
2025-04-22 07:31:46 +1000

    Prettier

--- jest.config.ts
+++ jest.config.ts
@@ -1,22 +1,24 @@
-import type { Config } from 'jest'
-import nextJest from 'next/jest.js'
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
+  dir: "./",
+});
 
 // Add any custom config to be passed to Jest
 const config: Config = {
-  coverageProvider: 'v8',
-  testEnvironment: 'jsdom',
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
   // Add more setup options before each test is run
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
-    "@/app": ["./app"]
+    "@/app": ["./app"],
     // "@/*": ["./*"]
-  }
-}
+  },
+  // Increase default timeout, in case need to manually wait for some async operations
+  testTimeout: 48000,
+};
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config)
+export default createJestConfig(config);
