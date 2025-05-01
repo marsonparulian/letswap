1bf3dbe6bc65f335ab87d2624bfd8e8b0a24218d
mpa <marson.parulian@gmail.com]>
2025-05-01 14:15:07 +1000

    Test: Modify script command

--- jest.config.ts
+++ jest.config.ts
@@ -18,6 +18,7 @@ const config: Config = {
   },
   // Increase default timeout, in case need to manually wait for some async operations
   testTimeout: 48000,
+  bail: true,
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
