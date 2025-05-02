a6b8eaca50ea72f6ddc05a176e846937bfa8f542
mpa <marson.parulian@gmail.com]>
2025-05-02 22:39:16 +1000

    Texts: add more error messages

--- app/lib/texts/texts.ts
+++ app/lib/texts/texts.ts
@@ -4,3 +4,18 @@
 export function fieldIsRequired(field: string): string {
   return `${field} is required`;
 }
+
+// Message when a field characters not meet the minimum
+export function fieldMinLength(field: string, min: number): string {
+  return `${field} must be at least ${min} characters`;
+}
+
+// Message when a field characters not meet the maximum
+export function fieldMaxLength(field: string, max: number): string {
+  return `${field} must be at most ${max} characters`;
+}
+
+// Message when a field chracters not in between min and max
+export function fieldLength(field: string, min: number, max: number): string {
+  return `${field} must be between ${min} and ${max} characters`;
+}
