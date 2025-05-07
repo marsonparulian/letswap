e8ab1245adc582457e62863139e4cdd50b6330fd
mpa <marson.parulian@gmail.com]>
2025-05-08 07:26:36 +1000

    Layout: Remove `async` in jQuery element

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import SiteNav from '@/app/ui/site-nav';
+import SiteNav from "@/app/ui/site-nav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,7 +15,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "LetSwap",
-  description: "Let's swap our collectibles! Promos, freebies, merch, giveaway, and all !",
+  description:
+    "Let's swap our collectibles! Promos, freebies, merch, giveaway, and all !",
 };
 
 export default function RootLayout({
@@ -29,18 +30,25 @@ export default function RootLayout({
         <header>
           <SiteNav />
         </header>
-        <main>
-          {children}
-        </main>
+        <main>{children}</main>
         {/* Foundation CSS */}
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.9.0/dist/css/foundation.min.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/foundation-sites@6.9.0/dist/css/foundation.min.css"
+        />
 
         {/* JQuery JS */}
-        <script async src="https://code.jquery.com/jquery-3.7.0.min.js" ></script>
+        <script
+          type="text/javascript"
+          src="https://code.jquery.com/jquery-3.7.0.min.js"
+        ></script>
 
         {/* Foundation JSF */}
-        <script defer src="https://cdn.jsdelivr.net/npm/foundation-sites@6.9.0/dist/js/foundation.min.js"></script>
-
+        <script
+          type="text/javascript"
+          defer
+          src="https://cdn.jsdelivr.net/npm/foundation-sites@6.9.0/dist/js/foundation.min.js"
+        ></script>
       </body>
     </html>
   );
