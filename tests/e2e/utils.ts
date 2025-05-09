09f343cb6011ac7db522528bdea95832069f8e9a
mpa <marson.parulian@gmail.com]>
2025-05-10 07:27:30 +1000

    Test: e2e: Fix path or browser's user data

--- tests/e2e/utils.ts
+++ tests/e2e/utils.ts
@@ -15,7 +15,7 @@ export const BASE_URL = "http://localhost:3000";
 // URL for testing
 export const URL_COLL_CREATE = `${BASE_URL}/collections/create`;
 // Browser's user data directory
-export const USER_DATA_DIR = "/home/mpa/projects/letswap/tests-e2e/user-data";
+export const USER_DATA_DIR = "/home/mpa/projects/letswap/tests/e2e/user-data";
 // Browser's options
 export const BROWSER_OPTIONS = {
   headless: false,
