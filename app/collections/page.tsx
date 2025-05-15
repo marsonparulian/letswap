06d7b8d883c1ab36122ed1e6214ce4f21a26cf8d
mpa <marson.parulian@gmail.com]>
2025-05-15 22:17:56 +1000

    Coll: List collection items

--- app/collections/page.tsx
+++ app/collections/page.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
+import { retrieve } from "@/app/lib/data/collections";
 
-export default function CollectionsPage() {
+export default async function CollectionsPage() {
   const links = [
     { href: "/collections/generate", label: "Generate collection" },
   ];
+
+  const colls: Collection[] = await retrieve();
   return (
     <>
-      <h1>List of Collectibles</h1>
+      <h1>
+        <span className="show-for-sr">List of</span> Collections
+      </h1>
       <div className="button-group align-right">
         {links.map(({ href, label }) => {
           return (
@@ -17,9 +22,16 @@ export default function CollectionsPage() {
         })}
       </div>
 
-      <div id="coll-list">
-        <ul>
-          <li></li>
+      <div id="coll-list" className="grid-container">
+        <ul className="grid-x grid-margin-x small-up-1 medium-up-2 large-up-4">
+          {colls.map((coll) => (
+            <li key={coll.id} className="cell card">
+              <div className="card-divider">
+                <h3>{coll.name}</h3>
+              </div>
+              <div className="card-section">{coll.description}</div>
+            </li>
+          ))}
         </ul>
       </div>
     </>
