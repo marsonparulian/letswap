3bd571c04cc4469850b5fe181ba2051681d63948
mpa <marson.parulian@gmail.com]>
2025-05-16 18:14:13 +1000

    Coll: DB: Retrieve collections DESC by `createdAt`

--- app/lib/data/collections.ts
+++ app/lib/data/collections.ts
@@ -1,60 +1,63 @@
 // Main data related functions to a specific table
-import { sql, tableToObject } from '@/app/lib/data/utils';
-import * as collItems from '@/app/lib/data/collection_items';
+import { sql, tableToObject } from "@/app/lib/data/utils";
+import * as collItems from "@/app/lib/data/collection_items";
 
-export const tableName = 'collections';
+export const tableName = "collections";
 // Type to represent a collection in DB table
 export type CollectionRow = CamelToSnake<Collection>;
 
-
 export async function deleteCollections(): Promise<void> {
-    await sql`DELETE FROM ${sql(tableName)}`;
+  await sql`DELETE FROM ${sql(tableName)}`;
 }
-
+// Retrieve collections from the latest creation time
 export async function retrieve(): Promise<Collection[]> {
-    const result: CollectionRow[] = await sql<CollectionRow[]>`SELECT * FROM ${sql(tableName)}`;
-    return result.map((row: CollectionRow) => {
-        return tableToObject(row);
-    });
+  const result: CollectionRow[] = await sql<
+    CollectionRow[]
+  >`SELECT * FROM ${sql(tableName)} ORDER BY created_at DESC`;
+  return result.map((row: CollectionRow) => {
+    return tableToObject(row);
+  });
 }
 
 export async function retrieveById(id: number): Promise<Collection | null> {
-    const [row] = await sql <CollectionRow[]>`
+  const [row] = await sql<CollectionRow[]>`
         SELECT * FROM ${sql(tableName)}
         WHERE id = ${id};
     `;
 
-    // Return if null
-    if (row === null) return null;
+  // Return if null
+  if (row === null) return null;
 
-    const coll = tableToObject(row);
+  const coll = tableToObject(row);
 
-    // attach collection items
-    coll.items = await collItems.retrieveByCollection(coll.id);
+  // attach collection items
+  coll.items = await collItems.retrieveByCollection(coll.id);
 
-    return coll;
+  return coll;
 }
 /**
  * Retrieve list of collection by keyword in  `slug` and `name`
  * @param keyword String to search the collections
- * @returns 
+ * @returns
  */
-export async function retrieveByKeyword(keyword: string): Promise<Collection[]> {
-    // Sanitize the keyword
-    const k = `%${keyword.replace(/[%_]/g, '\\$&')}%`;
+export async function retrieveByKeyword(
+  keyword: string
+): Promise<Collection[]> {
+  // Sanitize the keyword
+  const k = `%${keyword.replace(/[%_]/g, "\\$&")}%`;
 
-    // Query the `k` in `slug` and `name`
-    const collRows: CollectionRow[] = await sql<CollectionRow[]>`
+  // Query the `k` in `slug` and `name`
+  const collRows: CollectionRow[] = await sql<CollectionRow[]>`
         SELECT * FROM ${sql(tableName)}
         WHERE slug LIKE ${k} OR name LIKE ${k};
     `;
 
-    const colls: Collection[] = collRows.map((row) => tableToObject(row));;
-    return colls;
+  const colls: Collection[] = collRows.map((row) => tableToObject(row));
+  return colls;
 }
 export async function createCollection(coll: Collection): Promise<Collection> {
-    // Do not save `id` and `createdAt` fields, they are auto-generated
-    const [createdRow] = await sql<CollectionRow[]>`
+  // Do not save `id` and `createdAt` fields, they are auto-generated
+  const [createdRow] = await sql<CollectionRow[]>`
                INSERT INTO ${sql(tableName)} 
         (slug, name, description, items_count, producer_id, image_url, year)
         VALUES (${coll.slug}, ${coll.name}, ${coll.description}, 
@@ -63,28 +66,30 @@ export async function createCollection(coll: Collection): Promise<Collection> {
         RETURNING *;
     `;
 
-    // Convert to the created `collection`
-    const createdCollection: Collection = tableToObject(createdRow);
+  // Convert to the created `collection`
+  const createdCollection: Collection = tableToObject(createdRow);
 
-    // Generate items based on the saved collection
-    createdCollection.items = generateCollectionItemsByNumber(createdCollection);
+  // Generate items based on the saved collection
+  createdCollection.items = generateCollectionItemsByNumber(createdCollection);
 
-    // Save the collection items
-    await collItems.createMany(createdCollection.items);
+  // Save the collection items
+  await collItems.createMany(createdCollection.items);
 
-    return createdCollection;
+  return createdCollection;
 }
 /**
  * Generate collection items by the value of `collection.itemsCount`
- * @param collection 
- * @returns 
+ * @param collection
+ * @returns
  */
-export function generateCollectionItemsByNumber(collection: Collection): CollectionItem[] {
-    return Array.from({ length: collection.itemsCount }, (_, index) => ({
-        id: 0,
-        collectionId: collection.id,
-        orderNumber: index + 1,
-        name: `${index + 1}`,
-        description: "",
-    }));
+export function generateCollectionItemsByNumber(
+  collection: Collection
+): CollectionItem[] {
+  return Array.from({ length: collection.itemsCount }, (_, index) => ({
+    id: 0,
+    collectionId: collection.id,
+    orderNumber: index + 1,
+    name: `${index + 1}`,
+    description: "",
+  }));
 }
