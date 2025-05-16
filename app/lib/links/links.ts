30efd9a4ccb9344572c3aa08ce1fc8c2874417f5
mpa <marson.parulian@gmail.com]>
2025-05-16 18:08:44 +1000

    Links: Omit using BASE_PATH, only relative pat

--- app/lib/links/links.ts
+++ app/lib/links/links.ts
@@ -1,13 +1,9 @@
 // This file contains links used in the web
 
-export const BASE_URL = (): string => {
-  return "http://localhost:3000";
-};
-
 export const COLL_PAGE = (slug: string): string => {
   return `/c/${slug}`;
 };
 
 export const COLL_CREATE = (): string => {
-  return `${BASE_URL()}/collections/create`;
+  return `/collections/create`;
 };
