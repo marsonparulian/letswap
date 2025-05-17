9e22f15c31f3a3e889af4d0ce6f4e15434bbbcf1
mpa <marson.parulian@gmail.com]>
2025-05-17 22:22:57 +1000

    Producer: add method to get producer by id

--- app/lib/data/producers.ts
+++ app/lib/data/producers.ts
@@ -1,55 +1,67 @@
-import { sql } from '@/app/lib/data/utils';
+import { sql } from "@/app/lib/data/utils";
 
 // Main data related functions to a specific table
-export const tableName = 'producers';
+export const tableName = "producers";
 
 export async function getProducers(): Promise<Producer[]> {
-    try {
-        const producers = await sql<Producer[]>`
+  try {
+    const producers = await sql<Producer[]>`
             SELECT * FROM ${sql(tableName)}
         `;
-        return producers;
-    } catch (error) {
-        console.error('Failed fetching producers', error);
-        throw new Error('Failed to fetch producers');
-    }
+    return producers;
+  } catch (error) {
+    console.error("Failed fetching producers", error);
+    throw new Error("Failed to fetch producers");
+  }
 }
 
 export async function createProducer(producer: Producer): Promise<Producer> {
-
-    try {
-        const [savedProducer] = await sql<Producer[]>`
+  try {
+    const [savedProducer] = await sql<Producer[]>`
             INSERT INTO ${sql(tableName)} (slug, name, description)
             VALUES (${producer.slug}, ${producer.name}, 
             ${producer.description})
             RETURNING *
         `;
-        return savedProducer;
-    } catch (error) {
-        console.error('Failed to create producer', error);
-        throw new Error('Failed to create producer');
-    }
+    return savedProducer;
+  } catch (error) {
+    console.error("Failed to create producer", error);
+    throw new Error("Failed to create producer");
+  }
 }
 
 export async function getProducerBySlug(slug: string): Promise<Producer> {
-    try {
-        const producers = await sql<Producer[]>`
+  try {
+    const producers = await sql<Producer[]>`
             SELECT * FROM ${sql(tableName)}
             WHERE slug = ${slug}
         `;
-        return producers[0];
-    } catch (error) {
-        console.error(`Failed fetching producer by slug :${slug}`, error);
-        throw new Error(`Failed to fetch producer by slug: ${slug}`);
-    }
+    return producers[0];
+  } catch (error) {
+    console.error(`Failed fetching producer by slug :${slug}`, error);
+    throw new Error(`Failed to fetch producer by slug: ${slug}`);
+  }
+}
+
+export async function getProducerById(id: number): Promise<Producer | null> {
+  try {
+    const [producer] = await sql<Producer[]>`
+            SELECT * FROM ${sql(tableName)}
+            WHERE id = ${id}
+        `;
+    return producer || null;
+  } catch (error) {
+    console.error(`Failed fetching producer by id: ${id}`, error);
+    throw new Error(`Failed to fetch producer by id: ${id}`);
+  }
 }
 
 export async function updateProducer(producer: Producer): Promise<boolean> {
-    try {
-        console.log('going to update producer');
-        console.log(producer);
+  try {
+    console.log("going to update producer");
+    console.log(producer);
 
-        const updatedProducer = await sql`
+    const updatedProducer = await sql`
         UPDATE ${sql(tableName)}
         SET
             slug = ${producer.slug},
@@ -59,25 +71,25 @@ export async function updateProducer(producer: Producer): Promise<boolean> {
         RETURNING *
         `;
 
-        console.log(`updatedProducer: ${updatedProducer}`);
-        console.log(updatedProducer)
-        return true;
-    } catch (error) {
-        console.error(`Error updating producer: ${producer}`);
-        console.error(error);
-        throw new Error(`Error updating producer: ${producer}`);;
-    }
+    console.log(`updatedProducer: ${updatedProducer}`);
+    console.log(updatedProducer);
+    return true;
+  } catch (error) {
+    console.error(`Error updating producer: ${producer}`);
+    console.error(error);
+    throw new Error(`Error updating producer: ${producer}`);
+  }
 }
 
 export async function deleteProducer(id: number) {
-    try {
-        return sql`
+  try {
+    return sql`
 DELETE FROM ${sql(tableName)}
 WHERE id = ${id}
 `;
-    } catch (error) {
-        console.error(`Error delete producer id ${id}`);
-        console.error(error);
-        throw new Error(`Error deleting producer id: ${id}`);
-    }
+  } catch (error) {
+    console.error(`Error delete producer id ${id}`);
+    console.error(error);
+    throw new Error(`Error deleting producer id: ${id}`);
+  }
 }
